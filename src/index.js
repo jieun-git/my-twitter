7680f4623c8548eb927a8d9bca7e1c2589adf767
jieun <forworkje21@gmail.com>
2023-07-18 13:16:03 +0900

    firebase 정의 및 사용

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
+// src/index.js
 
- import React from 'react';
- import ReactDOM from 'react-dom/client';
- import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import firebase from "./firebase";
 
- const root = ReactDOM.createRoot(document.getElementById('root'));
- root.render(
-   <React.StrictMode>
-     <App />
-   </React.StrictMode>
- );
+console.log('firebase', firebase)
 
-
-
-
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 
 
