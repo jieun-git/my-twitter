18b83f407b06fef7b302d74963b262f162e4cf18
jieun <forworkje21@gmail.com>
2023-07-19 15:33:30 +0900

    Remove : 사용하지 않는 코드 제거

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
-import firebase from "./firebase";
-
-console.log('firebase', firebase)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
