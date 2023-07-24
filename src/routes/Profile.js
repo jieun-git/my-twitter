bfc20a9ee2755c1761b784cbd0e8f394f00441c7
jieun <forworkje21@gmail.com>
2023-07-24 21:13:08 +0900

    Refactor : Profile 컴포넌트에 로그아웃 버튼 추가 및 리다리엑트 설정

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,3 +1,20 @@
-const Profile = () => <span>Profile</span>
+// routes/Profile.js
+
+import { useNavigate } from 'react-router-dom'
+import { authService } from "../fbase"
+
+const Profile = () => {
+    const navigate = useNavigate()
+    const onLogout = () => {
+        authService.signOut().then(() => navigate('/'))
+    }
+
+
+    return(
+        <>
+            <button onClick={onLogout}>Log Out</button>
+        </>
+    )
+}
 
 export default Profile
