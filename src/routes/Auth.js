aeba81222b1df3f326504f36364c052775d4fdc5
jieun <forworkje21@gmail.com>
2023-07-24 21:12:29 +0900

    Refactor : 에러메세지 화면에 띄우기, 로그인/계정 생성 버튼 토글처리, 소셜 로그인 클릭 이벤트 로직 정의

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,12 +1,13 @@
 // src/routes/Auth.js
 
 import { useState } from "react";
-import { authService } from "../fbase";
+import {authService, firebaseInstance} from "../fbase";
 
 const Auth = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-    const [newAccount, setNewAccount] = useState(false)
+    const [newAccount, setNewAccount] = useState(true)
+    const [error, setError] = useState("")
 
     const onChange = (event) => {
         const {
@@ -30,35 +31,55 @@ const Auth = () => {
                 console.log('login data', data)
             }
         } catch (e) {
-            console.log('[ERR] AUTH SERVICE CALL', e)
+           setError(e.message)
         }
     }
 
+    const toggleAccount = () => setNewAccount((prev) => !prev)
+
+    const onSocialClick = async (event) => {
+        const {
+            target: {name}
+        } = event
+        let provider
+        if (name === 'google') {
+            provider = new firebaseInstance.auth.GoogleAuthProvider()
+        } else if (name === 'github'){
+            provider = new firebaseInstance.auth.GithubAuthProvider()
+        }
+
+        const data = await authService.signInWithPopup(provider)
+        console.log('data', data)
+    }
+
     return(
-        <div>
-            <form onSubmit={onSubmit}>
-                <input
-                    // onChange 함수는 하나, password inpu t과의 구분을 위해 name 속성 필요
-                    name="email"
-                    type="text"
-                    placeholder="Email"
-                    value={email}
-                    onChange={onChange}
-                    required />
-                <input
-                    name="password"
-                    type="password"
-                    placeholder="Password"
-                    value={password}
-                    onChange={onChange}
-                    required />
-                <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
-            </form>
+        <>
             <div>
-                <button>Continue with Google</button>
-                <button>Continue with Github</button>
+                <form onSubmit={onSubmit}>
+                    <input
+                        name="email"
+                        type="text"
+                        placeholder="Email"
+                        value={email}
+                        onChange={onChange}
+                        required />
+                    <input
+                        name="password"
+                        type="password"
+                        placeholder="Password"
+                        value={password}
+                        onChange={onChange}
+                        required />
+                    <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
+                    {error}
+                </form>
+                <span onClick={toggleAccount}>{newAccount ? "Sign In" : "Create Account"}</span>
+                <div>
+                    <button name="google" onClick={onSocialClick}>Continue with Google</button>
+                    <button name="github" onClick={onSocialClick}>Continue with Github</button>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
