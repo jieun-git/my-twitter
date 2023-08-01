b48643f6f52e13e12de7c4215550a2cbfb5e225f
jieun <forworkje21@gmail.com>
2023-08-01 11:50:08 +0900

    Refactor : 내 트윗을 얻어노는 함수 구현

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,13 +1,30 @@
 // routes/Profile.js
 
 import { useNavigate } from 'react-router-dom'
-import { authService } from "../fbase"
+import {authService, dbService} from "../fbase"
+import {useEffect} from "react";
 
-const Profile = () => {
+const Profile = ({ userObj }) => {
     const navigate = useNavigate()
     const onLogout = () => {
         authService.signOut().then(() => navigate('/'))
     }
+
+    const getMyTweets = async() => {
+        const tweets =
+            await dbService
+                .collection('tweets')
+                .where('creatorId', '==', userObj.uid)
+                .orderBy('createdAt')
+                .get()
+
+        console.log('#tweets', tweets.docs.map((doc) => doc.data()))
+    }
+
+
+    useEffect(() => {
+        getMyTweets()
+    }, [])
 
 
     return(
