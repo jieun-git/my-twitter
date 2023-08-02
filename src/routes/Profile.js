ebd08873f95c869963cc47a8fdd24e90419a42a2
jieun <forworkje21@gmail.com>
2023-08-02 16:44:18 +0900

    Add : 유저 프로필 업데이트 로직 기능 추가

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,11 +1,13 @@
 // routes/Profile.js
 
 import { useNavigate } from 'react-router-dom'
-import {authService, dbService} from "../fbase"
-import {useEffect} from "react";
+import { authService, dbService } from "../fbase"
+import { useEffect, useState } from "react";
 
-const Profile = ({ userObj }) => {
+const Profile = ({ userObj, refreshUser }) => {
     const navigate = useNavigate()
+
+    const [newDisplayName, setNewDisplayName] = useState(userObj.displayName)
     const onLogout = () => {
         authService.signOut().then(() => navigate('/'))
     }
@@ -26,9 +28,34 @@ const Profile = ({ userObj }) => {
         getMyTweets()
     }, [])
 
+    const onChange = (event) => {
+        const {
+            target: { value }
+        } = event
+        setNewDisplayName(value)
+    }
+
+    const onSubmit = async (event) => {
+        event.preventDefault()
+        if (userObj.displayName !== newDisplayName) {
+            await userObj.updateProfile({
+                displayName: newDisplayName
+            })
+            refreshUser()
+        }
+    }
 
     return(
         <>
+            <form onSubmit={onSubmit}>
+                <input
+                    type="text"
+                    placeholder="Display name"
+                    onChange={onChange}
+                    value={newDisplayName}
+                />
+                <input type="submit" value="Update Profile" />
+            </form>
             <button onClick={onLogout}>Log Out</button>
         </>
     )
