2391a0748c17dff332cd57957c1efccb303a987e
jieun <forworkje21@gmail.com>
2023-08-08 20:49:28 +0900

    Add : 생년월일 select 안의 값을 위한 상수 정의

---
+++ src/common/constants.js
@@ -0,0 +1,11 @@
+export const YEAR = []
+export const MONTH = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 12 ]
+export const DAY = []
+
+for (let i=1923; i<=2023; i++) {
+    YEAR.push(i)
+}
+
+for (let i=1; i<=31; i++) {
+    DAY.push(i)
+}
