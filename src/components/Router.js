c472ddb34b380c547b8e6231c7c292b5184d81a3
jieun <forworkje21@gmail.com>
2023-08-08 20:48:33 +0900

    Refactor : AuthForm 페이지 -> 모달 컴포넌트로 변경으로 인한 라우팅 처리 취소

--- src/components/Router.js
+++ src/components/Router.js
@@ -5,11 +5,8 @@ import Auth from "../routes/Auth";
 import Home from "../routes/Home";
 import Navigation from "components/Navigation";
 import Profile from "../routes/Profile";
-import AuthForm from "./AuthForm";
-import {useState} from "react";
 
 const AppRouter = ({ isLoggedIn, userObj, refreshUser }) => {
-    const [newAccount, setNewAccount] = useState(true)
     const setDisplayName = () => {
         if (!userObj.displayName) {
             userObj.displayName = userObj.email.split('@')[0]
@@ -29,8 +26,7 @@ const AppRouter = ({ isLoggedIn, userObj, refreshUser }) => {
                         </>
                         :
                         <>
-                            <Route path="/" element={<Auth setNewAccount={setNewAccount}/>} />
-                            <Route path="/account" element={<AuthForm newAccount={newAccount}/>} />
+                            <Route path="/" element={<Auth />} />
                         </>
                 }
             </Routes>
