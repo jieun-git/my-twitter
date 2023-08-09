ad1e9d1bf175fb8920b801d29832cb46c3850dcb
jieun <forworkje21@gmail.com>
2023-08-09 20:46:31 +0900

    Refactor : 메뉴 리스트 추가

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,7 +3,7 @@
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTwitter } from "@fortawesome/free-brands-svg-icons";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faUser, faHouse, faSearch, faBell, faEnvelope, faBookmark } from "@fortawesome/free-solid-svg-icons";
 
 
 const Navigation = ({ userObj }) => {
@@ -11,16 +11,45 @@ const Navigation = ({ userObj }) => {
         <nav>
             <ul className="navi-ul">
                 <li>
-                    <Link to="/" className="navi-logo">
-                        <FontAwesomeIcon icon={faTwitter} color="#04AAFF" size="2x" />
+                        <Link to="/" className="navi-logo">
+                            <FontAwesomeIcon icon={faTwitter} color="#04AAFF" size="4x" />
+                        </Link>
+                </li>
+                <div>
+                    <li>
+                        <Link to="/" className="navi-profile">
+                            <FontAwesomeIcon icon={faHouse} color="white" size="2x" />
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/" className="navi-profile">
+                            <FontAwesomeIcon icon={faSearch} color="white" size="2x" />
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/" className="navi-profile">
+                            <FontAwesomeIcon icon={faBell} color="white" size="2x" />
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/" className="navi-profile">
+                            <FontAwesomeIcon icon={faBookmark} color="white" size="2x" />
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/" className="navi-profile">
+                            <FontAwesomeIcon icon={faEnvelope} color="white" size="2x" />
+                        </Link>
+                    </li>
+                </div>
+                <li>
+                    <Link to="/profile" className="navi-profile">
+                        <FontAwesomeIcon icon={faUser} color="#04AAFF" size="2x" />
+                        <span>
+                            {/*{userObj.displayName} 's Profile*/}
+                        </span>
                     </Link>
                 </li>
-                <li><Link to="/profile" className="navi-profile">
-                    <FontAwesomeIcon icon={faUser} color="#04AAFF" size="2x" />
-                    <span>
-                        {userObj.displayName} 's Profile
-                    </span>
-                </Link></li>
             </ul>
         </nav>
     )
