c2372b633853df5060dccd2c257f6762d8f14b22
jieun <forworkje21@gmail.com>
2023-08-10 17:34:45 +0900

    Refactor : 메뉴에 로그아웃 버튼 추가

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,57 +1,66 @@
 // components/Navigation.js
 
-import { Link } from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTwitter } from "@fortawesome/free-brands-svg-icons";
-import { faUser, faHouse, faSearch, faBell, faEnvelope, faBookmark } from "@fortawesome/free-solid-svg-icons";
+import { faUser, faHouse, faSearch, faBell, faEnvelope, faBookmark, faPersonRunning } from "@fortawesome/free-solid-svg-icons";
 
 
 const Navigation = ({ userObj }) => {
+    const navigate = useNavigate()
+    const goLogout = () => {
+        navigate('/logout')
+    }
+
     return (
-        <nav>
-            <ul className="navi-ul">
-                <li>
+        <>
+            <nav>
+                <ul className="navi-ul">
+                    <li>
                         <Link to="/" className="navi-logo">
                             <FontAwesomeIcon icon={faTwitter} color="#04AAFF" size="4x" />
                         </Link>
-                </li>
-                <div>
-                    <li>
-                        <Link to="/" className="navi-profile">
-                            <FontAwesomeIcon icon={faHouse} color="white" size="2x" />
-                        </Link>
                     </li>
+                    <div>
+                        <li>
+                            <Link to="/" className="navi-profile">
+                                <FontAwesomeIcon icon={faHouse} color="white" size="2x" />
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/" className="navi-profile">
+                                <FontAwesomeIcon icon={faSearch} color="white" size="2x" />
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/" className="navi-profile">
+                                <FontAwesomeIcon icon={faBell} color="white" size="2x" />
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/" className="navi-profile">
+                                <FontAwesomeIcon icon={faBookmark} color="white" size="2x" />
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/" className="navi-profile">
+                                <FontAwesomeIcon icon={faEnvelope} color="white" size="2x" />
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/profile" className="navi-profile">
+                                <FontAwesomeIcon icon={faUser} color="white" size="2x" />
+                            </Link>
+                        </li>
+                    </div>
                     <li>
-                        <Link to="/" className="navi-profile">
-                            <FontAwesomeIcon icon={faSearch} color="white" size="2x" />
-                        </Link>
+                    <span onClick={goLogout}>
+                        <FontAwesomeIcon icon={faPersonRunning} color="darkGray" size="2x" />
+                    </span>
                     </li>
-                    <li>
-                        <Link to="/" className="navi-profile">
-                            <FontAwesomeIcon icon={faBell} color="white" size="2x" />
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/" className="navi-profile">
-                            <FontAwesomeIcon icon={faBookmark} color="white" size="2x" />
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/" className="navi-profile">
-                            <FontAwesomeIcon icon={faEnvelope} color="white" size="2x" />
-                        </Link>
-                    </li>
-                </div>
-                <li>
-                    <Link to="/profile" className="navi-profile">
-                        <FontAwesomeIcon icon={faUser} color="#04AAFF" size="2x" />
-                        <span>
-                            {/*{userObj.displayName} 's Profile*/}
-                        </span>
-                    </Link>
-                </li>
-            </ul>
-        </nav>
+                </ul>
+            </nav>
+        </>
     )
 }
 
